61a49b228fe3c49eeef7000cdadb3a5529a1023c
ManishPatidar <manishpatidar1180@gmail.com>
2025-03-02 00:54:07 +0530

    Initial commit

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run the JAR files in the background, each on a different port
+nohup java -jar all-jars/AuthenticationService-0.0.1-SNAPSHOT.jar --server.port=8081 > logs/Auth.log 2>&1 &
+nohup java -jar all-jars/CartHistoryService-0.0.1-SNAPSHOT.jar --server.port=8086 > logs/CartHistory.log 2>&1 &
+nohup java -jar all-jars/ForgotPasswordService-0.0.1-SNAPSHOT.jar --server.port=8085 > logs/ForgotPassowrd.log 2>&1 &
+nohup java -jar all-jars/Payment-0.0.1-SNAPSHOT.jar --server.port=8083 > logs/payment.log 2>&1 &
+nohup java -jar all-jars/ProductService-0.0.1-SNAPSHOT.jar --server.port=8082 > logs/Product.log 2>&1 &
+nohup java -jar all-jars/ReviewService-0.0.1-SNAPSHOT.jar --server.port=8084 > logs/Review.log 2>&1 &
+
+
+# Wait command to keep the script running and keep services active
+wait
