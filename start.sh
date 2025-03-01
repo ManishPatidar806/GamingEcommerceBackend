19bb79efa034db5775bd51ede4c7ef81d213c816
ManishPatidar <manishpatidar1180@gmail.com>
2025-03-02 02:01:36 +0530

    Some changes in gateway

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-nohup java -jar all-jars/API-Gateway-0.0.1-SNAPSHOT.jar --server.port=8080 > apigateway/Auth.log 2>&1 &
+nohup java -jar all-jars/API-Gateway-0.0.1-SNAPSHOT.jar --server.port=8080 > logs/apigateway.log 2>&1 &
 # Run the JAR files in the background, each on a different port
 nohup java -jar all-jars/AuthenticationService-0.0.1-SNAPSHOT.jar --server.port=8081 > logs/Auth.log 2>&1 &
 nohup java -jar all-jars/CartHistoryService-0.0.1-SNAPSHOT.jar --server.port=8086 > logs/CartHistory.log 2>&1 &
-nohup java -jar all-jars/ForgotPasswordService-0.0.1-SNAPSHOT.jar --server.port=8085 > logs/ForgotPassowrd.log 2>&1 &
+nohup java -jar all-jars/ForgotPasswordService-0.0.1-SNAPSHOT.jar --server.port=8085 > logs/ForgotPassword.log 2>&1 &
 nohup java -jar all-jars/Payment-0.0.1-SNAPSHOT.jar --server.port=8083 > logs/payment.log 2>&1 &
 nohup java -jar all-jars/ProductService-0.0.1-SNAPSHOT.jar --server.port=8082 > logs/Product.log 2>&1 &
 nohup java -jar all-jars/ReviewService-0.0.1-SNAPSHOT.jar --server.port=8084 > logs/Review.log 2>&1 &
